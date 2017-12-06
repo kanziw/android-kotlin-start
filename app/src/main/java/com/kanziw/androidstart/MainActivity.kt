33558f8c5be62f70b3d1ce705f81dfcec6c4f8b5
David Jung <david.jung@peeper.co>
2017-12-06 17:49:21 +0900

    merge 를 이용해 버튼 이벤트를 text 에 표시하도록 추가

--- app/src/main/java/com/kanziw/androidstart/MainActivity.kt
+++ app/src/main/java/com/kanziw/androidstart/MainActivity.kt
@@ -3,6 +3,8 @@ package com.kanziw.androidstart
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.jakewharton.rxbinding2.view.RxView
+import com.jakewharton.rxbinding2.widget.RxTextView
+import io.reactivex.Observable
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.rxkotlin.addTo
 import kotlinx.android.synthetic.main.activity_main.*
@@ -14,7 +16,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        RxView.clicks(button_inc).subscribe({ println("") }).addTo(disposeBag)
+        val streamInc = RxView.clicks(button_inc).map { 1 }//.subscribe({ println(" +") }).addTo(disposeBag)
+        val streamDec = RxView.clicks(button_dec).map { -1 }//.subscribe({ println(" -") }).addTo(disposeBag)
+
+        Observable.merge(streamInc, streamDec)
+                .map { it.toString() }
+                .subscribe(RxTextView.text(main_text))
+                .addTo(disposeBag)
     }
 
     override fun onDestroy() {
