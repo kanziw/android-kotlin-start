cdbf0459c8a3a7d10ac798c1bb20807210ce8ff0
David Jung <david.jung@peeper.co>
2017-12-06 17:22:16 +0900

    disposeBag 추가

--- app/src/main/java/com/kanziw/androidstart/MainActivity.kt
+++ app/src/main/java/com/kanziw/androidstart/MainActivity.kt
@@ -3,13 +3,21 @@ package com.kanziw.androidstart
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.jakewharton.rxbinding2.view.RxView
+import io.reactivex.disposables.Disposable
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private var disposeBag: Disposable? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        RxView.clicks(button).subscribe({ println("") })
+        disposeBag = RxView.clicks(button).subscribe({ println("") })
+    }
+
+    override fun onDestroy() {
+        disposeBag?.dispose()
+        super.onDestroy()
     }
 }
