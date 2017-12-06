6e948dddea0b9639ccc8e8c0581b1a390231e253
David Jung <david.jung@peeper.co>
2017-12-06 17:19:08 +0900

    button click event 를 Rx 로 변경

--- app/src/main/java/com/kanziw/androidstart/MainActivity.kt
+++ app/src/main/java/com/kanziw/androidstart/MainActivity.kt
@@ -2,6 +2,7 @@ package com.kanziw.androidstart
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.jakewharton.rxbinding2.view.RxView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -9,6 +10,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        button.setOnClickListener { println("") }
+        RxView.clicks(button).subscribe({ println("") })
     }
 }
