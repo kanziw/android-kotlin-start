9c00f6dc0c4e976d2915bfd2ac7b73e833e72fce
David Jung <david.jung@peeper.co>
2017-12-06 17:35:34 +0900

    + / - string resource 추가 및 - 버튼 추가

--- app/src/main/java/com/kanziw/androidstart/MainActivity.kt
+++ app/src/main/java/com/kanziw/androidstart/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        RxView.clicks(button).subscribe({ println("") }).addTo(disposeBag)
+        RxView.clicks(button_inc).subscribe({ println("") }).addTo(disposeBag)
     }
 
     override fun onDestroy() {
