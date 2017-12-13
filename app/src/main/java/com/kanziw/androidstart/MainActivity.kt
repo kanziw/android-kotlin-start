8eb307f3e0a271917e54bc845d65ff670fb36333
David Jung <david.jung@peeper.co>
2017-12-13 19:04:20 +0900

    Tab 터치에 대한 동작을 isAdded & isHidden 을 이용해 관리하도록 수정

--- app/src/main/java/com/kanziw/androidstart/MainActivity.kt
+++ app/src/main/java/com/kanziw/androidstart/MainActivity.kt
@@ -10,42 +10,54 @@ import kotlinx.android.synthetic.main.bottom_tab_layout.*
 class MainActivity : AppCompatActivity() {
 
     private val disposeBag = CompositeDisposable()
+    private lateinit var counterFragment: CounterFragment
+    private var todoListFragment: ToDoListFragment? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        counterFragment = CounterFragment.newInstance()
+
         supportFragmentManager.beginTransaction()
-                .add(R.id.main_view, CounterFragment.newInstance())
+                .add(R.id.main_view, counterFragment)
                 .commit()
 
         RxView.clicks(bottom_tab_1)
                 .subscribe {
-                    val fm = supportFragmentManager
+                    val ft = supportFragmentManager.beginTransaction()
 
-                    val currentFragment = fm.findFragmentById(R.id.main_view)
-                    if (currentFragment !is CounterFragment) {
-                        fm.fragments.find { it is CounterFragment }?.let {
-                            fm.beginTransaction().show(it).hide(currentFragment).commit()
-                        } ?: run {
-                            fm.beginTransaction().add(R.id.main_view, CounterFragment.newInstance()).hide(currentFragment).commit()
-                        }
+                    if (counterFragment.isHidden) {
+                        ft.show(counterFragment)
                     }
+
+                    if (todoListFragment?.isAdded == true) {
+                        ft.hide(todoListFragment)
+                    }
+
+                    ft.commit()
                 }
                 .addTo(disposeBag)
 
         RxView.clicks(bottom_tab_2)
                 .subscribe {
-                    val fm = supportFragmentManager
+                    val ft = supportFragmentManager.beginTransaction()
 
-                    val currentFragment = fm.findFragmentById(R.id.main_view)
-                    if (currentFragment !is ToDoListFragment) {
-                        fm.fragments.find { it is ToDoListFragment }?.let {
-                            fm.beginTransaction().show(it).hide(currentFragment).commit()
-                        } ?: run {
-                            fm.beginTransaction().add(R.id.main_view, ToDoListFragment.newInstance()).hide(currentFragment).commit()
-                        }
+                    if (todoListFragment == null) {
+                        todoListFragment = ToDoListFragment.newInstance()
                     }
+
+                    if (todoListFragment?.isAdded == false) {
+                        ft.add(R.id.main_view, todoListFragment)
+                    } else {
+                        ft.show(todoListFragment)
+                    }
+
+                    if (counterFragment.isAdded) {
+                        ft.hide(counterFragment)
+                    }
+
+                    ft.commit()
                 }
                 .addTo(disposeBag)
     }
