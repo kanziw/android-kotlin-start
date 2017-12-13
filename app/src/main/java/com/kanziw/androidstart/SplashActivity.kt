68f25bb79dd8d06a5ee7bed90ffda950773884fc
David Jung <david.jung@peeper.co>
2017-12-13 14:40:18 +0900

    Splash 화면 추가

---
+++ app/src/main/java/com/kanziw/androidstart/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.kanziw.androidstart
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import io.reactivex.Observable
+import java.util.concurrent.TimeUnit
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Observable.just(0)
+                .delay(1, TimeUnit.SECONDS)
+                .subscribe {
+                    startActivity(Intent(this, MainActivity::class.java))
+                }
+    }
+}
