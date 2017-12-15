f7845825861a52c98233fc39b4d66893f89a28b4
David Jung <david.jung@peeper.co>
2017-12-15 12:16:59 +0900

    2번 탭에서 화면을 가로로 돌리면 1번 탭이 중복으로 뜨는 버그. 강제로 세로 고정 시킴

--- app/src/main/java/com/kanziw/androidstart/MainActivity.kt
+++ app/src/main/java/com/kanziw/androidstart/MainActivity.kt
@@ -20,7 +20,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val initialMainViewFragment = CounterFragment.newInstance()
-
         supportFragmentManager.beginTransaction()
                 .add(R.id.main_view, initialMainViewFragment)
                 .commit()
